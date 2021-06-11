85bddb4cc0020294d74506bb954d5e8348efd9be
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2021-06-11 06:35:18 +0100

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"𝐆𝐫𝐨𝐮𝐩:- @Cinemahub182\n𝐑𝐞𝐬𝐮𝐥𝐭𝐬 𝐅𝐨𝐮𝐧𝐝:- <code>{leng}</code>\n𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞:- <code>{query}</code>"
+    text=f"Found {(len_results)}\n Results For Your Query: <code>{query}</code>\n"
         
     try:
         await update.message.edit(
