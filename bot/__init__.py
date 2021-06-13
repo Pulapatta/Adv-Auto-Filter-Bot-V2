e6106d9c488d90981ab9176a13a8eb9cc431052b
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2021-06-13 13:59:18 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,6 +13,8 @@ from .translation import Translation
 # Change Accordingly While Deploying To A VPS
 APP_ID = int(os.environ.get("APP_ID"))
 
+FORCESUB_CHANNEL = os.environ.get("FORCESUB_CHANNEL", "")
+
 API_HASH = os.environ.get("API_HASH")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
