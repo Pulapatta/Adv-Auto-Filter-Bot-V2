b860ceea0d454679dced7656180179d47e6f7dba
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2021-06-13 14:12:24 +0100

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -33,7 +33,7 @@ Join on our channel to get movies ✅
 
 ⬇️Channel link⬇️ </b>""",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="⚡ Join My Channel⚡️", url=f"https://t.me/{FORCESUB_CHANNEL}")]
+                    [ InlineKeyboardButton(text="⚡ Join My Channel⚡️", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
