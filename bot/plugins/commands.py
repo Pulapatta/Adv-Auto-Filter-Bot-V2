c7c43630a084d32edf77fa9c607b74890eb5aded
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2021-06-23 18:28:35 +0100

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,8 +57,7 @@ Join on our channel to get movies ✅
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"<code>{file_name}\n▱▰▱▰▱▰▱▰▱▰▱▰▱▰\n➠ɢʀᴏᴜᴘ   : @Cinemahub182 🍿\n➠ɢʀᴏᴜᴘ   : @Cinemahub182 🍿\n➥ᴄʜᴀɴɴᴇʟ: @cinemAhub186 \n⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉⑉\n🎗 ʝσιи 🎗 ѕнαяє🎗 ѕυρρσят 🎗</code>"
-,
+                caption = f"<code>{file_name}</code><b>Join us:--> @Cinemahub182</b>",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
