dd4715c7830c6111b91fcca8d90ca9264ed57b4c
Pulapatta <87922252+Pulapatta@users.noreply.github.com>
2021-08-20 14:46:29 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1608,7 +1608,7 @@ async def cb_about(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/Anandhukuttu"
+                    "My Dev ⚡", url="https://t.me/hindinewmoviesdo"
                 ),
                 
             InlineKeyboardButton
