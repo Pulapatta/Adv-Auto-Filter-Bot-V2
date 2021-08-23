52c0898151d487dd2b62bb975ebe6b4092dd34b9
Pulapatta <87922252+Pulapatta@users.noreply.github.com>
2021-08-23 21:23:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -54,7 +54,7 @@ Join on our channel to get movies ✅
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"<code>{📁 file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code></code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                
+                caption = f"<code>{📁file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code></code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -92,7 +92,7 @@ Join on our channel to get movies ✅
             await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
-                caption = f"<code>{file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                 
+                caption = f"<code>{📁file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code></code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                 
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
@@ -128,7 +128,7 @@ Join on our channel to get movies ✅
             await bot.send_audio(
                 chat_id=update.chat.id,
                 audio = file_id,
-                caption = f"<code>{file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                
+                caption = f"<code>{📁file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>f<code></code>\n \n<b>♻️Join with us: @Hollywoodare\n \n♻️Join with us: @Hollywoodare</b>",                
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
