7fe04e9b8860de69bdf514845f7b17711b0b2f65
Pulapatta <87922252+Pulapatta@users.noreply.github.com>
2021-08-23 18:44:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -55,7 +55,7 @@ Join on our channel to get movies ✅
                 chat_id=update.chat.id,
                 document = file_id,
                 caption = f"<code>{file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>",
-f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
+                caption =f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -94,7 +94,7 @@ f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
                 chat_id=update.chat.id,
                 video = file_id,
                 caption = f"<code>{file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>",
-f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
+                caption =f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
@@ -131,7 +131,7 @@ f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
                 chat_id=update.chat.id,
                 audio = file_id,
                 caption = f"<code>{file_name}</code>\n \n<b>♻️Join with us: @MovieRosters</b>",
-f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
+                caption =f"<code>{file_name}</code>\n \n<b>♻️Join with us: @Hollywoodare</b>",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
