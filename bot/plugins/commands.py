44906ab31e7420696feeee38f26ce5cb0f8af9bf
Pulapatta <87922252+Pulapatta@users.noreply.github.com>
2021-10-18 12:17:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -116,15 +116,20 @@ async def about(bot, update):
         InlineKeyboardButton('𝘾𝙇𝙊𝙎𝙀 🔐', callback_data='close')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
+    )
     
-    @@ -124,4 +124,4 @@ async def about(bot, update):
+await bot.send_message(
+
+        chat_id=update.chat.id,
+
+        text=Translation.ABOUT_TEXT,
+
+        reply_markup=reply_markup,
 
         disable_web_page_preview=True,
 
         parse_mode="html",
 
         reply_to_message_id=update.message_id
-
-    )u
 
     )
